a46c7665b523479e06dc3d08473c28d1f4ae3a82
Aayush Upreti <136055230+ayus-10@users.noreply.github.com>
2024-01-18 18:13:13 +0545

    install package react-intersection-observer

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import SocialLinks from "./SocialLinks";
 
 const Hero = () => {
   return (
-    <div className="w-screen flex-grow bg-gray-200" id="hero">
-      Hero
+    <div className="relative w-screen flex-grow bg-gray-200" id="hero">
+      <SocialLinks />
     </div>
   );
 };
--- src/components/ScrollTop.jsx
+++ src/components/ScrollTop.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { MdKeyboardArrowUp } from "react-icons/md";
 import { Link } from "react-scroll";
 
-const ScrollTop = () => {
+const ScrollTop = ({ invertColor }) => {
   return (
     <Link
-      className="fixed bottom-[4vh] right-[2vw] cursor-pointer rounded-full bg-purple-600 text-5xl hover:bg-purple-700 md:text-6xl"
+      className="fixed bottom-[4vh] right-[2vw] cursor-pointer rounded-full bg-purple-600 text-5xl duration-200 ease-in-out hover:bg-purple-700 md:text-6xl"
       to="home"
       spy={true}
       smooth={true}
