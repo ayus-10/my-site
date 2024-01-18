d21b858c3edaea8205c833b2cb83333922883dd3
Aayush Upreti <136055230+ayus-10@users.noreply.github.com>
2024-01-18 17:48:46 +0545

    create component

---
+++ src/components/SocialLinks.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { TbBrandGithubFilled } from "react-icons/tb";
+import { LiaLinkedinIn } from "react-icons/lia";
+import { SiMinutemailer } from "react-icons/si";
+
+const SocialLinks = () => {
+  const socialLinks = [
+    {
+      url: "https://github.com/ayus-10",
+      icon: <TbBrandGithubFilled />,
+      color: "#171515",
+    },
+    {
+      url: "https://www.linkedin.com/in/aayush-upreti-a21945293/",
+      icon: <LiaLinkedinIn />,
+      color: "#0072b1",
+    },
+    {
+      url: "mailto:contact@aayushupreti.com.np",
+      icon: <SiMinutemailer />,
+      color: "#db4437",
+    },
+  ];
+
+  return (
+    <div className="absolute bottom-[4vh] left-[2vw] flex flex-col gap-2">
+      {socialLinks.map((link, index) => (
+        <div
+          className="hover:animate-wiggle rounded-full p-2"
+          style={{ backgroundColor: link.color }}
+          key={index}
+        >
+          <a className="text-3xl text-white md:text-4xl" href={link.url}>
+            {link.icon}
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SocialLinks;
