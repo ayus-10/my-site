87fd6e8677e1273f251aa2e75dc50792efb09cb1
Aayush Upreti <136055230+ayus-10@users.noreply.github.com>
2024-01-18 22:02:47 +0545

    minor style changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,15 +8,15 @@ const Hero = () => {
       className="relative flex max-h-screen w-screen flex-grow bg-gray-200 px-4"
       id="hero"
     >
-      <div className="-mt-[3.25rem] flex flex-col-reverse justify-center gap-4 md:flex-row md:items-center">
+      <div className="-mt-[3.25rem] flex flex-col-reverse justify-center gap-4 md:flex-row md:items-center md:gap-8">
         <div className="w-full text-center md:w-1/2 md:text-right">
-          <h2 className="text-3xl font-semibold text-purple-950 md:text-3xl">
+          <h2 className="text-2xl font-semibold text-purple-950 md:text-3xl">
             Hey, there!
           </h2>
-          <h1 className="my-2 text-4xl font-bold text-purple-600 md:text-5xl">
-            Aayush Upreti, this side.
+          <h1 className="text-3xl font-bold text-purple-600 md:my-2 md:text-5xl">
+            Aayush Upreti this side.
           </h1>
-          <p className="text-lg md:text-xl">
+          <p className="md:text-xl">
             I am a student passionate about computers and technology. I have
             developed strong skills in frontend development with React JS and
             currently I'm learning fullstack development with Node.
@@ -25,7 +25,7 @@ const Hero = () => {
         <img
           src={image}
           alt="My image"
-          className="mx-auto h-auto w-48 rounded-full md:mx-0 md:w-96 md:rounded-3xl"
+          className="mx-auto h-auto w-48 rounded-full border-2 border-dashed border-purple-600 md:mx-0 md:w-96 md:rounded-3xl md:border-4"
         />
       </div>
       <SocialLinks />
