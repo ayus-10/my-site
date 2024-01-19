f18810fe05f5d771877e8c9ac5b7d3e14beb39f1
Aayush Upreti <136055230+ayus-10@users.noreply.github.com>
2024-01-19 17:29:13 +0545

    create component Skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useRef } from "react";
+import { skillItems } from "../data/SkillItems.jsx";
 
 const Skills = () => {
+  const itemsRef = useRef();
+
+  const handleHover = (id, color) => {
+    const items = itemsRef.current;
+    const item = items.querySelector(`#${id}`);
+    item.style.color = color;
+    if (Array.from(item.classList).includes("animate-shake")) {
+      item.classList.remove("animate-shake");
+    } else {
+      item.classList.add("animate-shake");
+    }
+  };
+
   return (
-    <div className="min-h-screen w-screen bg-gray-200" id="skills">
-      Skills
+    <div className="min-h-screen w-screen bg-gray-100" id="skills">
+      <div className="px-4 py-12">
+        <h1 className="text-center text-3xl font-bold text-purple-600 md:my-2 md:text-5xl">
+          Which tools can I work with?
+        </h1>
+        <div
+          className="mx-auto grid max-w-[1280px] grid-cols-2 gap-12 py-16 md:grid-cols-3 md:py-24 lg:grid-cols-4"
+          ref={itemsRef}
+        >
+          {skillItems.map((item, index) => {
+            const id = `item${index}`;
+            return (
+              <div
+                id={id}
+                key={index}
+                className="flex flex-col items-center justify-center text-gray-500"
+                onMouseOver={() => handleHover(id, item.color)}
+                onMouseOut={() => handleHover(id, null)}
+              >
+                <div className="text-8xl md:text-9xl">{item.image}</div>
+                <span className="cursor-default text-lg md:text-3xl md:font-bold">
+                  {item.name}
+                </span>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/data/SkillItems.jsx
@@ -0,0 +1,49 @@
+import { FaHtml5, FaReact, FaPython } from "react-icons/fa6";
+import { RiJavascriptFill } from "react-icons/ri";
+import { IoLogoCss3 } from "react-icons/io";
+import { FaGitAlt } from "react-icons/fa";
+import { SiExpress } from "react-icons/si";
+import { DiMongodb } from "react-icons/di";
+
+export const skillItems = [
+  {
+    name: "HTML",
+    image: <FaHtml5 />,
+    color: "#f06529",
+  },
+  {
+    name: "CSS",
+    image: <IoLogoCss3 />,
+    color: "#2965f1",
+  },
+  {
+    name: "JavaScript",
+    image: <RiJavascriptFill />,
+    color: "#f7df1e",
+  },
+  {
+    name: "React JS",
+    image: <FaReact />,
+    color: "#00d8ff",
+  },
+  {
+    name: "Python",
+    image: <FaPython />,
+    color: "#4584b6",
+  },
+  {
+    name: "Git",
+    image: <FaGitAlt />,
+    color: "#c9510c",
+  },
+  {
+    name: "Express JS",
+    image: <SiExpress />,
+    color: "#333",
+  },
+  {
+    name: "Mongo DB",
+    image: <DiMongodb />,
+    color: "#589636",
+  },
+];
