a4bf75a059401936f67ac8af9e5f5a0b5ac88823
Aayush Upreti <136055230+ayus-10@users.noreply.github.com>
2024-02-02 12:59:52 +0545

    style changes

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -14,7 +14,7 @@ const Notification = ({ setNotification, status, message }) => {
 
   return (
     <div
-      className={`absolute right-1/2 top-12 translate-x-1/2 rounded-lg px-2 py-2 font-bold text-white md:px-4 md:text-lg ${status ? "bg-green-500" : "bg-red-500"}`}
+      className={`absolute right-1/2 top-4 w-full translate-x-1/2 rounded-lg px-2 py-2 text-center font-bold text-white md:w-2/3 md:px-4 md:text-lg ${status ? "bg-green-500" : "bg-red-500"}`}
     >
       <span>{message}</span>
     </div>
