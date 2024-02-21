af7f97f9a789ff222270b4f198c0d8c8bdb5467a
Aayush Upreti <136055230+ayus-10@users.noreply.github.com>
2024-02-21 17:02:46 +0545

    updates

--- src/data/SkillItems.jsx
+++ src/data/SkillItems.jsx
@@ -1,17 +1,12 @@
 import { FaReact, FaPython } from "react-icons/fa6";
 import { BiLogoTypescript } from "react-icons/bi";
 import { RiJavascriptFill } from "react-icons/ri";
-import { IoLogoCss3 } from "react-icons/io";
 import { FaGitAlt } from "react-icons/fa";
 import { SiExpress } from "react-icons/si";
 import { DiMongodb } from "react-icons/di";
+import { TbBrandNextjs } from "react-icons/tb";
 
 export const skillItems = [
-  {
-    name: "CSS",
-    image: <IoLogoCss3 />,
-    color: "#2965f1",
-  },
   {
     name: "JavaScript",
     image: <RiJavascriptFill />,
@@ -28,14 +23,10 @@ export const skillItems = [
     color: "#00d8ff",
   },
   {
-    name: "Python",
-    image: <FaPython />,
-    color: "#4584b6",
-  },
-  {
-    name: "Git",
-    image: <FaGitAlt />,
-    color: "#c9510c",
+    name: "Next JS",
+    image: <TbBrandNextjs />,
+    color: "#333",
+    altColor: "#fff",
   },
   {
     name: "Express JS",
@@ -47,5 +38,15 @@ export const skillItems = [
     name: "Mongo DB",
     image: <DiMongodb />,
     color: "#589636",
+  },
+  {
+    name: "Python",
+    image: <FaPython />,
+    color: "#4584b6",
+  },
+  {
+    name: "Git",
+    image: <FaGitAlt />,
+    color: "#c9510c",
   },
 ];
