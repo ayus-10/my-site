3efa8997719fd9d07c3f1b17abda88779d27667b
Aayush Upreti <136055230+ayus-10@users.noreply.github.com>
2024-03-04 12:09:26 +0545

    add new project details

--- src/data/ProjectItems.js
+++ src/data/ProjectItems.js
@@ -1,14 +1,23 @@
+import anyflex1 from "../assets/anyflex1.png";
+import anyflex2 from "../assets/anyflex2.png";
 import quizzy1 from "../assets/quizzy1.png";
 import quizzy2 from "../assets/quizzy2.png";
 import quizzy3 from "../assets/quizzy3.png";
-import splash1 from "../assets/splash1.jpg";
-import splash2 from "../assets/splash2.jpg";
-import splash3 from "../assets/splash3.jpg";
 import x1 from "../assets/x1.jpg";
 import x2 from "../assets/x2.jpg";
 import x3 from "../assets/x3.jpg";
 
 const projectItems = [
+  {
+    title: "Anime Showcase Site",
+    description:
+      "A Next.js app that lets users explore and save favorite anime to their watchlist, users being authenticated via Discord OAuth.",
+    images: [anyflex1, anyflex2],
+    links: {
+      code: "https://github.com/ayus-10/any-flex",
+      demo: "https://any-flex.vercel.app/",
+    },
+  },
   {
     title: "Quiz App",
     description:
@@ -17,16 +26,6 @@ const projectItems = [
     links: {
       code: "https://github.com/ayus-10/quizzy-frontend",
       demo: "https://quizzy-frontend-flax.vercel.app/",
-    },
-  },
-  {
-    title: "Portfolio Page",
-    description:
-      "A React JS powered personal portfolio template - includes a custom image carousel for showcasing your projects.",
-    images: [splash1, splash2, splash3],
-    links: {
-      code: "https://github.com/ayus-10/splash",
-      demo: "https://splash-liart.vercel.app/",
     },
   },
   {
